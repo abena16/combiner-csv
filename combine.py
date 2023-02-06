82cb71e277fbba6364cc2f280a85477b182b96fe
Arturo Benavides <arturobenavides792@gmail.com>
2023-02-05 23:54:50 -0600

    added ifelse statement check

--- combine.py
+++ combine.py
@@ -3,22 +3,24 @@ import pandas as pd
 import sys
 import os
 
-#creates list that will contain our command line arguments
+#creates empty list that will contain our command line arguments
 file_list = []
 
-#loop through the arguments
+#loop through the arguments 
 for datafile in sys.argv[1:]:
 
-    #adds the arguments to my empty list + used 'os' to get the base name in specified path. 
+    #adds the arguments to our empty list + used 'os' to get the base name in specified path. 
     file_list.append(pd.read_csv(datafile).assign(filename = os.path.basename(datafile)))
 
-    
+if len(file_list) > 1:
     #concat function to combine the files
     combine_data = pd.concat(file_list)
 
     #save our new combined csv data into a new file 
     combine_data.to_csv("combined.csv")
+    print("Operation successful.")
 
-
+else:
+    print("Invalid Arguments.")
 
 
